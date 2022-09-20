a99b9e73343fd21c6ccbb7c0b677951f4f63ee97
iamclock <double7@mail.ru>
2022-09-20 23:32:53 +0700

    added images as a buttons and animations examples

--- example.py
+++ example.py
@@ -4,8 +4,52 @@ from kivy.properties import ObjectProperty
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.tabbedpanel import TabbedPanel
+from kivy.animation import Animation
 
 
+# animations example exercise 36
+class CustLayout(Widget):
+
+    def do_callback(self, *args):
+        self.ids.cust_label.text = "What a Sh*t!"
+        return
+
+    def buttn_released(self, widget, *args):
+        animate = Animation(background_color=(0, 0, 1, 1), duration=2)
+        animate += Animation(size_hint=(1, 1))
+        animate += Animation(size_hint=(.5, .5))
+        animate.start(widget)
+        animate.bind(on_complete=self.do_callback)
+        return
+
+
+class ExampleApp(App):
+    pass
+
+
+if __name__ == '__main__':
+    ExampleApp().run()
+'''
+# imgs as a buttns example exercise 35
+class CustomLayout(Widget):
+
+    def buttn_pressed(self):
+        self.ids.cust_label.text = "Pressed"
+        self.ids.cust_img.source = "imgs/btn_pressed.jpg"
+
+    def buttn_released(self):
+        self.ids.cust_label.text = "Unpressed"
+        self.ids.cust_img.source = "imgs/btn.jpg"
+
+
+class ExampleApp(App):
+    pass
+
+
+if __name__ == '__main__':
+    ExampleApp().run()
+'''
+'''
 # Tabs example exercise 34
 class ExampleApp(App):
 
@@ -16,6 +60,7 @@ class ExampleApp(App):
 
 if __name__ == '__main__':
     ExampleApp().run()
+'''
 '''
 # Splitters example exercise 33
 class SplittersEx(Widget):
